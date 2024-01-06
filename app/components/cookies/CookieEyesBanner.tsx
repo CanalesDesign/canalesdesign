2bff043cf6f907f48757de69be5df0907999e1d0
canalesid <rodrigo@canalesid.com>
2024-01-06 10:39:20 -0300

    Build 1.16 Fix Errors

--- app/components/cookies/CookieEyesBanner.tsx
+++ app/components/cookies/CookieEyesBanner.tsx
@@ -2,11 +2,10 @@ import Script from 'next/script';
 
 export default function CookieEyesBanner() {
   return (
-    <div className="cookieeyes__banner--wrapper">
-      <Script
-        src={`https://cdn-cookieyes.com/client_data/a340b124c2c84dfdf9263c17/script.js`}
-        strategy="beforeInteractive"
-      />
-    </div>
+    <Script
+      id="cookies__eyes--script"
+      src={`https://cdn-cookieyes.com/client_data/a340b124c2c84dfdf9263c17/script.js`}
+      strategy="beforeInteractive"
+    />
   );
 }
